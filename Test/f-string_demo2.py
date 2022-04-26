97985e2b5a9c26aacee0414422fe9aec76967428
Administrator <11>
2022-04-27 01:44:34 +0900

    some add

--- Test/f-string_demo2.py
+++ Test/f-string_demo2.py
@@ -1,5 +1,11 @@
 prices = input('please input some number\n').split()
-for price in prices:
-    afterTax = int(price)*1.1
-    print(f'before tex is {price},and after tex is {afterTax}')
 
+
+def calcTax(price):
+    afterTax = int(price) * 1.1
+    print(f'before tex is {price},and after tex is {afterTax}')
+    return afterTax
+
+
+for p in prices:
+    calcTax(p)
