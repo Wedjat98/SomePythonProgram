ab19028fb4e3575fa3a70e31f35d6fd1d53c3704
Administrator <11>
2022-04-27 09:55:35 +0900

    some add

--- Start/class_demo.py
+++ Start/class_demo.py
@@ -23,8 +23,24 @@ class Benz2018(BenzCar):
     price = 880000
     model = 'Benz2018'
 
+    def __init__(self, color, engineSN, weight):
+        # 先调用父类的初始化方法
+        super().__init__(color, engineSN)
+        self.weight = weight  # 车的重量
+        self.oil_weight = 0  # 油的重量
+
+    # 加油
+    def fillOil(self, oilAdded):
+        self.oil_weight += oilAdded
+        self.weight += oilAdded
+
 
 car1 = BenzCar('白色', '24503425527866')
 car1.changeColor('黑色')
-
+car2 = Benz2018('blue', '111135545988', 1500)
 print(car1.color)
+print(car2.oil_weight)
+print(car2.weight)
+car2.fillOil(50)
+print(car2.oil_weight)
+print(car2.weight)
